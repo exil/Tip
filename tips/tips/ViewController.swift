1e7744b7e371cc2657eda8e2d1eef3f7c26bd433
Max Pappas <mpappas>
2016-01-04 03:06:08 -0800

    added currency settings

--- tips/tips/ViewController.swift
+++ tips/tips/ViewController.swift
@@ -11,16 +11,21 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var tipLabel: UILabel!
-    @IBOutlet var billField: UITextField!
+
     @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var billField: UITextField!
     @IBOutlet var tipControl: UISegmentedControl!
     @IBOutlet var tipSlider: UISlider!
     @IBOutlet var tipSliderLabel: UILabel!
     @IBOutlet var totalView: UIView!
     @IBOutlet var tipView: UIView!
+    @IBOutlet var currencyLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setDefaults()
+        
         // Do any additional setup after loading the view, typically from a nib.
         let notificationCenter = NSNotificationCenter.defaultCenter()
         notificationCenter.addObserver(self, selector: "applicationResigned", name: UIApplicationWillResignActiveNotification, object: nil)
@@ -31,36 +36,47 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setDefaults() {
+        // sets default tip values if not set already
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var defaultTipValue: AnyObject? = defaults.objectForKey("defaultTipValue")
+        
+        if (defaultTipValue == nil) {
+            defaults.setInteger(15, forKey: "minimumTipValue")
+            defaults.setInteger(25, forKey: "maximumTipValue")
+            defaults.setInteger(20, forKey: "defaultTipValue")
+        }
+    }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
         var tipPercentage = roundf(tipSlider.value)
-        var billText = billField.text.stringByReplacingOccurrencesOfString("$", withString: "")
-        
+        var locale = NSLocale.currentLocale()
+        var formatter = NSNumberFormatter()
+        var billText = billField.text
         var billAmount = (billText as NSString).doubleValue
+        
+        println(billText)
+        println(billField.frame.origin.y)
+        
+        // format billText for regional purposes
+        var billFormatted = NSNumberFormatter().numberFromString(billText)
+        if let billFormatted = billFormatted {
+            billAmount = Double(billFormatted)
+        }
+        
         var tip = billAmount * Double(tipPercentage / 100)
         var total = billAmount + tip
         
         println("\(billField.text) \(tip) \(total)")
         
-        tipLabel.text = String(format: "+ $%.2f", tip)
-        totalLabel.text = String(format: "= $%.2f", total)
+        formatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        formatter.locale = locale
+        tipLabel.text = formatter.stringFromNumber(tip)
+        totalLabel.text = formatter.stringFromNumber(total)
         tipSliderLabel.text = "\(Int(tipPercentage))%"
     }
 
-    /*@IBAction func animateViews(sender: AnyObject) {
-        UIView.animateWithDuration(0.2, animations: {
-            var totalViewFrame = self.totalView.frame
-            var tipViewFrame = self.tipView.frame
-            
-            totalViewFrame.origin.y -= self.view.bounds.width
-            tipViewFrame.origin.y -= self.view.bounds.width
-            
-            self.totalView.frame = totalViewFrame
-            self.tipView.frame = tipViewFrame
-            }, completion: { finished in
-                
-        })
-    }*/
     
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
@@ -73,6 +89,8 @@ class ViewController: UIViewController {
         var defaultTipValue = defaults.integerForKey("defaultTipValue")
         var minimumTipValue = defaults.integerForKey("minimumTipValue")
         var maximumTipValue = defaults.integerForKey("maximumTipValue")
+        
+        println("\(defaultTipValue)")
         
         tipSlider.minimumValue = Float(minimumTipValue)
         tipSlider.maximumValue = Float(maximumTipValue)
@@ -92,13 +110,9 @@ class ViewController: UIViewController {
     }
     
     @IBAction func billAmountChanged(sender: AnyObject) {
-        if (count(billField.text) < 1) {
-            billField.text = "$"
-        }
-        
-        // if just $, reset to initial view
+        // if empty, reset to initial view
         // once user has typed something, switch to full view
-        if (count(billField.text) == 1) {
+        if (count(billField.text) == 0) {
             resetPositions(true)
         } else {
             setPositions(true)
@@ -108,40 +122,40 @@ class ViewController: UIViewController {
     /* move everything into place */
     func setPositions(doAnimate: Bool) {
         let animationDuration = doAnimate ? 0.2 : 0.0
-        let billFieldOrigin: CGFloat = 90
-        let totalViewOrigin: CGFloat = 233
-        let tipViewOrigin: CGFloat = 63
+        let billFieldOriginY: CGFloat = 90
+        let totalViewOriginY: CGFloat = 233
+        let tipViewOriginY: CGFloat = 63
         
         // if amount view is already full height, just return
-        if (billField.frame.origin.y == billFieldOrigin) {
+        if (billField.frame.origin.y == billFieldOriginY) {
             return;
         }
         
         UIView.animateWithDuration(animationDuration, animations: {
-            println(self.totalView.frame.origin.y)
-            println(self.tipView.frame.origin.y)
-            self.totalView.frame.origin.y = totalViewOrigin
-            self.tipView.frame.origin.y = tipViewOrigin
+            self.totalView.frame.origin.y = totalViewOriginY
+            self.tipView.frame.origin.y = tipViewOriginY
             
             //self.amountView.frame.size.height = amountViewHeight
-            self.billField.frame.origin.y = billFieldOrigin
+            self.billField.frame.origin.y = billFieldOriginY
+            self.currencyLabel.frame.origin.y = billFieldOriginY
+            self.currencyLabel.alpha = 0
         })
     }
     
     func resetPositions(doAnimate: Bool) {
         let animationDuration = doAnimate ? 0.2 : 0.0
+        let billFieldOriginY: CGFloat = 200
         // if frames are already out of view, just return
         if (self.totalView.frame.origin.y > self.view.bounds.width) {
             return;
         }
         
         UIView.animateWithDuration(animationDuration, animations: {
-            println(self.totalView.frame.origin.y)
-            println(self.tipView.frame.origin.y)
             self.totalView.frame.origin.y += self.view.bounds.height
             self.tipView.frame.origin.y += self.view.bounds.height
-            self.billField.frame.origin.y = 293
-            println(self.billField.frame.origin.y)
+            self.billField.frame.origin.y = billFieldOriginY
+            self.currencyLabel.frame.origin.y = billFieldOriginY
+            self.currencyLabel.alpha = 1
         })
     }
     
@@ -179,8 +193,14 @@ class ViewController: UIViewController {
             }
         }
         
+        // set currency symbol
+        var locale = NSLocale.currentLocale()
+        var formatter = NSNumberFormatter()
+        formatter.locale = locale
+        currencyLabel.text = formatter.currencySymbol
+        
         // don't do animations on first load
-        if (count(billField.text) == 1) {
+        if (count(billField.text) == 0) {
             resetPositions(false)
         } else {
             setPositions(false)
